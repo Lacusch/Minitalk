12a629485032e98c1bed2560e26989a83c0e28fd
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-08-22 17:49:38 +0200

    Made a working client prototype

--- test/client.c
+++ test/client.c
@@ -6,7 +6,7 @@
 /*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 12:23:20 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/08/16 12:24:21 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/08/22 17:49:14 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,19 @@
 
 int main (int argc, char* argv[])
 {
-	if (argc > 3)
-		{
-			printf("Too manz arguments");
-			return (1);
-		}
-	argc[1][]
+	int pid;
+	
+	if (argc != 3)
+	{
+		printf ("error, format correctly");
+		return (1);
+	}
+	pid = atoi(argv[1]);
+	if (atoi (argv[2]) == 1)
+		kill (pid, SIGUSR1);
+	else if (atoi(argv[2]) == 2)
+		kill (pid, SIGUSR2);
+	else
+		printf("Error, try again");
+	return (0);
 }
