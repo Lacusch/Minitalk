3332825c0c1508fd454590af6b8bc4555e3aeed8
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-08-22 20:17:40 +0200

    Test client changes (added a loop(

--- test/client.c
+++ test/client.c
@@ -6,7 +6,7 @@
 /*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 12:23:20 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/08/22 17:49:14 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/08/22 17:57:13 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,27 @@
 int main (int argc, char* argv[])
 {
 	int pid;
-	
-	if (argc != 3)
-	{
-		printf ("error, format correctly");
-		return (1);
-	}
+	int counter = -1;
+	// if (argc != 3)
+	// {
+	// 	printf ("error, format correctly");
+	// 	return (1);
+	// }
 	pid = atoi(argv[1]);
-	if (atoi (argv[2]) == 1)
+	while (counter != 8)
+	{
 		kill (pid, SIGUSR1);
-	else if (atoi(argv[2]) == 2)
-		kill (pid, SIGUSR2);
-	else
-		printf("Error, try again");
+	}
+	if (counter == 8)
+	{
+		counter = -1;
+		return (0);
+	}
+	// if (atoi (argv[2]) == 1)
+	// 	kill (pid, SIGUSR1);
+	// else if (atoi(argv[2]) == 2)
+	// 	kill (pid, SIGUSR2);
+	// else
+	// 	printf("Error, try again");
 	return (0);
 }
