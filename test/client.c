ca68f1cbbd67fcacfac24416686f08c5c47fa736
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-08-23 18:13:24 +0200

    revriting test funcions a bit

--- test/client.c
+++ test/client.c
@@ -6,7 +6,7 @@
 /*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 12:23:20 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/08/22 17:57:13 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/08/23 13:13:21 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,26 @@
 
 int main (int argc, char* argv[])
 {
-	int pid;
+	int s_pid;
+	int c_pid;
 	int counter = -1;
+
+	c_pid = getpid();
+	printf("my pid is:%i", c_pid);
 	// if (argc != 3)
 	// {
 	// 	printf ("error, format correctly");
 	// 	return (1);
 	// }
-	pid = atoi(argv[1]);
-	while (counter != 8)
+	s_pid = atoi(argv[1]);
+	while (++counter != 8)
 	{
-		kill (pid, SIGUSR1);
+		kill (s_pid, SIGUSR1);
+		counter++;
 	}
 	if (counter == 8)
 	{
-		counter = -1;
+		counter = 0;
 		return (0);
 	}
 	// if (atoi (argv[2]) == 1)
@@ -37,5 +42,4 @@ int main (int argc, char* argv[])
 	// 	kill (pid, SIGUSR2);
 	// else
 	// 	printf("Error, try again");
-	return (0);
 }
--- test/server.c
+++ test/server.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 15:28:18 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/08/16 12:23:08 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/08/23 13:13:31 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,31 @@
 
 void signal_handler(int signum)
 {
-	static unsigned char c = 0xFF;
+	static char  c = 0xFF;
 	static int bits = 0;
 	printf ("number: %i\n", c);
 	printf ("bits: %i\n", bits);
- 	if (signum == SIGUSR1)
+	if (signum == SIGUSR1)
 	{
- 		printf("got signal 1 which means '1'\n");
+		printf("got signal 1 which means '1'\n");
 		c ^= 0x80 >> bits;
 	}
- 	else if (signum == SIGUSR2)
+	else if (signum == SIGUSR2)
 	{
- 		printf ("got signal 2 which means '0'\n");
+		printf ("got signal 2 which means '0'\n");
 		c |= 0x80 >> bits;
 	}
 	bits++;
 	if (bits == 8)
 	{
-		write (1, &c, 1);
-		bits = 0;
-		c = 0xFF;
+	printf("The charactes is %c", c);
+	bits = 0;
+	c = 0xFF;
 	}
 }
 
 int main (int argc, char *argv[])
 {
-	// char a = 0xFF;
-	// char b = 0xF0;
-	// int i = 0;
 	pid_t	pid;
 	pid = getpid();
 	printf("PID: %d\n", pid);
